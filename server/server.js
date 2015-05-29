d442b44f0cfc7d36aa3d9b65013ea6bbfc8fa0d2
Alex Mendez <Alex@Alexs-MacBook-Pro.local>
2015-05-29 12:38:53 -0700

    refactor(server): reverted back to standard express server from socket.io

--- server/server.js
+++ server/server.js
@@ -2,25 +2,5 @@
 * Express server running at http://localhost:3000
 * TODO: add routing
 * */
-
 var express = require('express');
 var app = express();
-var server = require('http').Server(app);
-var io = require('socket.io').listen(server);
-
-var firebaseRequestHandler = require('./middleware/authFirebase');
-
-app.set('port', process.env.PORT || 3000);
-app.use(express.static('public'));
-
-//static files will be served from the public directory
-app.use(function (req, res, next) {
-  var ts = new Date();
-  console.log(req.url + ' - ' + req.method);
-  console.log('Time:', ts);
-  next();
-});
-
-server.listen(app.get('port'), function() {
-  console.log('Express server with socket.io intiated')
-})
